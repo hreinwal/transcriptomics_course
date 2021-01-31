c57c01b02284c02dedb63c811c78bf45498905a7
Reinwald <hannes.reinwald@ime.fraunhofer.de>
2021-01-31 23:43:25 +0100

    New upload!

---
+++ packageInstaller.R
@@ -0,0 +1,24 @@
+### Installing required R packages for course ###
+
+# Author: Hannes Reinwald
+# Contact: hannes.reinwald@ime.fraunhofer.de
+
+# README # ---------------------------------------------------------
+# Small script to quickly equip the student's R environment with the
+# useful and required packages for the ecotox transcriptomics course.
+####################################################################
+
+## Bioconductor source packages ##
+BioconPackage = c("DESeq2","IHW","ComplexHeatmap",
+                  "apeglm","qvalue",
+                  "EnhancedVolcano","pcaMethods",
+                  "clusterProfiler","biomaRt")
+BiocManager::install(BioconPackage, ask = F)
+
+## CRAN source packages ##
+CranP = c("RColorBrewer","viridis","dplyr","ggplot2",
+          "ggpubr","hexbin","Rtsne")
+for(i in CranP) install.packages(i)
+rm(i)
+
+### END OF SCRIPT ###
